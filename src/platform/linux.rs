16387d5d3f466160608b5c2833f76f01142565ab
Corey Richardson <corey@octayn.net>
2015-05-26 18:58:41 -0400

    [fix] Don't try to use the generic ioctl encoding on unknown archs

--- src/platform/linux.rs
+++ src/platform/linux.rs
@@ -29,7 +29,12 @@ mod consts {
     #[doc(hidden)]
     pub const DIRBITS: u8 = 3;
 }
-#[cfg(not(any(target_arch = "powerpc", target_arch = "mips")))]
+
+#[cfg(not(any(target_arch = "powerpc", target_arch = "mips", target_arch = "x86", target_arch = "arm", target_arch = "x86_64", target_arch = "aarch64")))]
+use this_arch_not_supported;
+
+// "Generic" ioctl protocol
+#[cfg(any(target_arch = "x86", target_arch = "arm", target_arch = "x86_64", target_arch = "aarch64"))]
 mod consts {
     #[doc(hidden)]
     pub const NONE: u8 = 0;
